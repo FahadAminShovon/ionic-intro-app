c126abdb59ff3e7ecafbb0878346f4fa100e8b2d
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2024-02-08 19:03:01 +0600

    implement animation

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,6 @@
 import {
+  CreateAnimation,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -6,10 +8,16 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonViewDidEnter,
 } from '@ionic/react';
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Tab2: React.FC = () => {
+  const animationRef = useRef<CreateAnimation | null>(null);
+
+  useIonViewDidEnter(() => {
+    // animationRef.current?.animation.play();
+  });
   return (
     <IonPage>
       <IonHeader>
@@ -20,7 +28,23 @@ const Tab2: React.FC = () => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>UI goes here...</IonContent>
+      <IonContent className='ion-padding'>
+        <CreateAnimation
+          duration={2000}
+          iterations={Infinity}
+          delay={1000}
+          ref={animationRef}
+          keyframes={[
+            { offset: 0, transform: 'scale(1)', opacity: '1' },
+            { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
+            { offset: 1, transform: 'scale(1)', opacity: '1' },
+          ]}
+        >
+          <IonButton color='success' className='ion-margin' expand='block'>
+            Test Ionic
+          </IonButton>
+        </CreateAnimation>
+      </IonContent>
     </IonPage>
   );
 };
